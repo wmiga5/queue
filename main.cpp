d6b1905953c448ceafbdcc01ea51d675dae51c9a
wojtek <wojtek.miga@gmail.com>
2023-03-31 11:11:06 +0200

    Dodany plik zdanie i pierwsze metody do niego

--- main.cpp
+++ main.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 #include "kolejka.h"
-
+#include "zdanie.h"
 using namespace std;
 
 int main() {
 
 
     Kolejka<string> kolej;
-    kolej.insert(1,"kloc1");
+    Zdanie wpis;
 
-    kolej.insert(3,"kloc2");
-    kolej.insert(2,"kloc3");
+    wpis.wpisz();
 
-    kolej.insert(5,"kloc4");
-    kolej.wypisz();
-    cout<<kolej.size();
-    kolej.removeMin();
-    cout<<endl;
-    kolej.wypisz();
-    cout<<kolej.size();
-    //cout<<kolej.min()<<endl;
+    wpis.podziel();
+
+
+
     return 0;
 }
